0111abf54e4cff19d377167a02d06674207fda76
clavero <jose.clavero.anderica@gmail.com>
2020-07-04 18:05:19 +0200

    Append zip function and Append method

--- collections/list.go
+++ collections/list.go
@@ -7,6 +7,13 @@ func GenerateList(items ...interface{}) ListType {
 	return items
 }
 
+//Foreach is the default
+func (list ListType) Foreach(fn func(interface{}, int))  {
+	for index, item := range list {
+		fn(item, index)
+	}
+}
+
 //Map is the default
 func (list ListType) Map(mapper func(interface{}, int) interface{}) ListType {
 	result := ListType{}
@@ -35,4 +42,42 @@ func (list ListType) Reverse() ListType {
 		res = append(res, list[index])
 	}
 	return res
+}
+
+
+
+//FilterAll is the default
+func (list ListType) FilterAll(fn func(interface{}) bool) ListType {
+	result := ListType{}
+	for _, item := range list {
+		if(fn(item)){
+			result = append(result, fn(item))
+		}
+	}
+	return result
+}
+
+//FilterFirst is the default
+func (list ListType) FilterFirst(fn func(interface{}) bool) (interface{}, int) {
+	for index:= 0; index < len(list); index++{
+		if(fn(list[index])){
+			return list[index], index
+		}
+	}
+	return nil, -1
+}
+
+//FilterLast is the default
+func (list ListType) FilterLast(fn func(interface{}) bool) (interface{}, int) {
+	for index:= len(list)- 1; index >= 0 ; index--{
+		if(fn(list[index])){
+			return list[index], index
+		}
+	}
+	return nil, -1
+}
+
+// Append is the default way to insesrt elements 
+func (list ListType) Append(item interface{}) ListType {
+	return append(list, item)
 }
--- main.go
+++ main.go
@@ -15,15 +15,24 @@ func main() {
 	examplesWithList()
 }
 
+
+
+
 func examplesWithList() {
 	//	var newList collections.ListType = collections.GenerateList(user{"Alvaro",6,1},user{"Sofia",3,2})
 	newList := collections.GenerateList(user{"Alvaro", 6, 1}, user{"Sofia", 3, 2})
 	results := newList.Map(mapperLst)
 	fmt.Println(results.Reverse().Join("(♥)"))
 	fmt.Println(results)
+
+	listTuples, _ := collections.Zip(results.Reverse(), results)
+	fmt.Println(listTuples.Join("(♥)"))
+
 }
 
 func mapperLst(mapper interface{}, index int) interface{} {
 	user1Item := mapper.(user)
 	return user1Item.name
 }
+
+
